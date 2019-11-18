be10f0cfb3c307f2b64e43d31e1b01fa79fb5902
vagrant <vagrant@localhost.localdomain>
2019-11-18 03:44:49 +0000

    add create user
    -muser作成機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,63 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user, {only: []}
+    before_action :forbid_login_user, {only: [:login_form, :login]}
+
+    def login_form
+
+    end
+
+    def login
+
+    end
+
+    def createStudent_form
+        @user = User.new
+    end
+
+    def createTeacher_form
+        @user = User.new
+    end
+
+    def createStudent
+        @user = User.new(
+            user_id: params[:user_id],
+            password: params[:password],
+            name: params[:name],
+            class_number: params[:class_number],
+            teacher_id: params[:teacher_id],
+            role: "student"
+        )
+        if @user.save
+            flash[:notice] = "学生登録が完了"
+            redirect_to("/index")
+        else
+            flash[:notice] = "登録内容に問題があります"
+            @user_id = params[:user_id]
+            @password = params[:password]
+            @name = params[:name]
+            @class_number = params[:class_number]
+            @teacher_id = params[:teacher_id]
+            render("users/createStudent_form")
+        end
+    end
+
+    def createTeacher
+        @user = User.new(
+            user_id: params[:user_id],
+            password: params[:password],
+            name: params[:name],
+            role: "teacher"
+        )
+        if @user.save
+            flash[:notice] = "教員登録が完了"
+            redirect_to("/index")
+        else
+            flash[:notice] = "登録内容に問題があります"
+            @user_id = params[:user_id]
+            @password = params[:password]
+            @name = params[:name]
+            render("users/createTeacher_form")
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "new/student" => "users#createStudent_form"
+  get "new/teacher" => "users#createTeacher_form"
+  post "new/student" => "users#createStudent"
+  post "new/teacher" => "users#createTeacher"
+
+  get "index" => "reports#index"
+  get "/" => "reports#index"
 end
